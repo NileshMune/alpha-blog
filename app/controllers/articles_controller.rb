733de43b63f8e84a744afe1a301dee408bd89f0f
Rails course <nileshmune@gmail.com>
2016-03-01 01:11:05 +0000

    Save and display articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,9 +4,17 @@ class ArticlesController < ApplicationController
   end
   def create 
     @article = Article.new(article_params)
-    @article.save
-    # redirect to show template
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      # redirect to show template
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+    @article = Article.find(params[:id])
   end
   
   private
